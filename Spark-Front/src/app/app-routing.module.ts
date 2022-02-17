543476136cdb3a233cc3f2b877f600659bfd3382
celiawork <celiawork@outlook.fr>
2022-02-17 15:39:38 +0100

    generate composant

--- Spark-Front/src/app/app-routing.module.ts
+++ Spark-Front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BottomPopupInteractiveComponent } from './components/bottom-popup-interactive/bottom-popup-interactive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "bottompopupinteractive", component: BottomPopupInteractiveComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Spark-Front/src/app/app.module.ts
+++ Spark-Front/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { BottomPopupInteractiveComponent } from './components/bottom-popup-interactive/bottom-popup-interactive.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    BottomPopupInteractiveComponent,
   ],
   imports: [
     BrowserModule,
