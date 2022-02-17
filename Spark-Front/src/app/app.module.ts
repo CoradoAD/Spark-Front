2a6f3cb529aedc1b50cd027dd4ac5f9eecd1213d
celiawork <celiawork@outlook.fr>
2022-02-17 15:28:46 +0100

    main corrigé

--- Spark-Front/src/app/app.module.ts
+++ Spark-Front/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
-import { ParkingComponent } from './shared/models/parking/parking.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    ParkingComponent
   ],
   imports: [
     BrowserModule,
