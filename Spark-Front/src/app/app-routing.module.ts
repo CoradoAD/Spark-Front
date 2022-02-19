4dd6d9707f6fa9d698ad59e7d402ca11b7d727b1
celiawork <celiawork@outlook.fr>
2022-02-19 17:40:37 +0100

    correction ok

--- Spark-Front/src/app/app-routing.module.ts
+++ Spark-Front/src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BottomPopupComponent } from './components/bottom-popup/bottom-popup.component';
 
-const routes: Routes = [
-  { path: "bottompopup", component: BottomPopupComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
