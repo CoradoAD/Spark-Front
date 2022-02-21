16c39b6f02d0a4001cbcd1deab4a7769c44a29c7
Brigitte <Brigitte@ITK-P-201701-03.ITK.local>
2022-02-21 12:35:50 +0100

    modele parking

--- Spark-Front/src/app/shared/models/parking.ts
+++ Spark-Front/src/app/shared/models/parking.ts
@@ -1,4 +1,20 @@
 // Interface for 'Parking' entity
 // Must be contaigned alls of 'parking-model' values
 export interface Parking {
+    idParking:BigInteger ;
+    name:string;
+    freeCapacity:BigInteger;
+    addressInfo:string;
+    nbPlaces:BigInteger;
+    xLong:number;
+    xLat:number;
+    apiUrl:string;
+    functionType:string;
 }
+    
+
+
+
+
+
+
